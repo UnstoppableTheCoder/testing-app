4ad4b4a87d9f4a8836b7d3a9680908b4ffb5de12
vipendra <vipendrasinghpatel999@gmail.com>
2026-03-06 22:05:41 +0530

    Add server code

---
+++ index.ts
@@ -0,0 +1,22 @@
+import os from "os";
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": new Response("This is the homepage"),
+    "/health-check": new Response("OK"),
+    "/hostname": new Response(`${os.hostname}`),
+    "/message": {
+      GET: () => Response.json({ message: "hello there" }),
+    },
+    "/compute": {
+      GET: () => {
+        for (let i = 0; i < 10000000000; i++) {}
+        return Response.json({ message: "Computation is done" });
+      },
+    },
+    "/*": Response.json({ message: "Not found" }, { status: 200 }),
+  },
+});
+
+console.log(`Listening on ${server.url}`);
